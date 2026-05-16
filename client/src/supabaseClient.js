be3a78d62be8a6e9f672382737c60e03fee9ca01
Rachanon Beche <rachanon.work2547@gmail.com>
2026-05-16 21:08:25 +1000

    changes from using supabase key to anon key

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Initialize the Supabase client
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
